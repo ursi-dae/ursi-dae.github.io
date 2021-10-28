56731eeeecc53cf56ac85f434e182da9affc9276
NEB1803 <83712440+NEB1803@users.noreply.github.com>
2021-10-28 18:54:32 +0530

    neb

---
+++ assets/js/aboutus.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+  AOS.init();
+  const eventswiper = document.querySelector(".swiper-wrapper");
+  const event = [
+    // eslint-disable-next-line linebreak-style
+    {
+      title: "Talk on ABC",
+      image: "assets/images/event/1.png",
+    },
+    {
+      title: "Keynote on XYZ",
+      image: "assets/images/event/2.png",
+    },
+    { 
+      title: "Group Picture", 
+      image: "assets/images/event/3.png" },
+    {
+      title: "Presentation",
+      image: "assets/images/event/4.png",
+    },
+    {
+      title: "Note on XYZ",
+      image: "assets/images/event/5.png",
+    },
+    {
+      title: "Keytopics",
+      image: "assets/images/event/6.png",
+    },
+    {
+      title: "The Gist",
+      image: "assets/images/event/7.png",
+    },
+    {
+      title: "Thank You",
+      image: "assets/images/event/8.png",
+    },
+  ];
+  // Filling the details from the object array event
+  function fillData() {
+    let output = "";
+    for (let x = 0; x < event.length; x++) {
+      output += `
+    <div class="swiper-slide">
+    <div class="img-container">
+      <img src=${event[x].image} alt="" class="img-fluid" />
+    </div>
+    <div class="container arrow_contain">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="wrapper">
+            <div class="content">
+              <div class="title">
+                <div class="text-wrapper">
+                  <div class="text-inner">
+                  ${event[x].title}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+         </div> 
+          <!-- wraper freee -->
+      </div>
+    </div>
+  </div>`;
+    }
+    eventswiper.innerHTML = output;
+  }
+  document.addEventListener("DOMContentLoaded", fillData());
+  const parallaxSliderOptions = {
+    speed: 1000,
+    effect: "coverflow",
+    autoplay: false,
+    parallax: true,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 30,
+      depth: 20,
+      stretch: 0,
+      modifier: 1,
+      slideShadows: true,
+    },
+    // parallax effect
+    on: {
+      init: function () {
+        const swiper = this;
+        for (let i = 0; i < swiper.slides.length; i++) {
+          $(swiper.slides[i])
+            .find(".img-container")
+            .attr({
+              "data-swiper-parallax": 0.75 * swiper.width,
+              "data-swiper-paralalx-opacity": 0.5,
+            });
+        }
+        const index = swiper.activeIndex;
+        $(swiper.slides).removeClass("active");
+        $(swiper.slides[index]).addClass("active");
+      },
+      transitionEnd: function () {
+        const swiper = this;
+        const index = swiper.activeIndex;
+        $(swiper.slides).removeClass("active");
+        $(swiper.slides[index]).addClass("active");
+      },
+      resize: function () {
+        this.update();
+      },
+    },
+    navigation: {
+      nextEl: ".image-slider .next-ctrl",
+      prevEl: ".image-slider .prev-ctrl",
+    },
+  };
+  // initializing swiper
+  const slider = new Swiper(".image-slider", parallaxSliderOptions);
+});
