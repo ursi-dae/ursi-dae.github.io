6c10bf0ded2847c913a6ce6493303e1eed86ec04
NEB1803 <83712440+NEB1803@users.noreply.github.com>
2021-10-30 19:38:27 +0530

    by NEB to add Map of Ursidae SMP

---
+++ assets/js/server.js
@@ -0,0 +1,16 @@
+var url = "https://api.minetools.eu/ping/185.248.134.134:11043"; //insert  server here
+
+$.getJSON(url, function(r) {
+  //data is the JSON string
+  if (r.error) {
+    $('#rest').html('Server Offline');
+    return false;
+  }
+  var pl = '';
+  if (r.players.sample.length > 0) {
+    pl = '<br>OP: ' + r.players.sample[0].name;
+  }
+  $('#rest').html(r.description.replace(/§(.+?)/gi, '') + '<br><b>Players Online:</b> ' + r.players.online + pl);
+  $('#favicon').attr('src', r.favicon);
+
+});
